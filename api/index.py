f835b053144217cc9e9450dfb73f4190b66bd545
Ronaldo dias de sousa <ronaldohunter54@gmail.com>
2025-05-25 16:00:22 -0300

    Comit app

--- api/index.py
+++ api/index.py
@@ -1,12 +1,6 @@
-import os
-from flask import Flask, render_template, request, redirect, url_for, flash, session, jsonify
 import sys
 sys.path.append('..')
 from app import app
 
-app = Flask(__name__)
-app.secret_key = os.environ.get('SECRET_KEY', 'chave_padrao_insegura')
-
-# Handler para Vercel
 def handler(environ, start_response):
     return app(environ, start_response) 
